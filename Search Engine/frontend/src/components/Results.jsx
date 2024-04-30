5d8bacedb08b3d1a5642d4885dd3374f0ecf6632
Youssef Roshdy <elroshdy23@gmail.com>
2024-05-01 02:06:18 +0300

    pagination for the results

--- Search Engine/frontend/src/components/Results.jsx
+++ Search Engine/frontend/src/components/Results.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect , useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import ReactPlayer from 'react-player';
 
@@ -9,26 +9,37 @@ export const Results = () => {
   const { results, loading, getResults, searchTerm } = useStateContext();
 
   const location =useLocation();
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const resultsPerPage = 10;
 
 
+  const indexOfLastResult = currentPage * resultsPerPage;
+  const indexOfFirstResult = indexOfLastResult - resultsPerPage;
+  const currentResults = results.slice(indexOfFirstResult, indexOfLastResult);
 
   useEffect(() => {
     
       if(searchTerm !=='')
         getResults(`?query=${searchTerm}=40&related_keywords=true`);
-      
+     
+    
   
   }, [searchTerm]);
 
-  
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+ 
+};
 
   if (loading) return <Loading />;
 
 
 console.log(results.results);
       return (
+        <div>
         <div className="sm:px-56 flex flex-wrap justify-between space-y-6 mt-10">
-          {results?.results?.map(({ url, title ,description}, index) => (
+          {currentResults?.map(({ url, title ,description}, index) => (
             <div key={index} className="md:w-2/5 w-full">
               <a href={url} target="_blank" rel="noreferrer">
                 <p className="text-sm">{url.length > 30 ? url.substring(0, 30) : url}</p>
@@ -36,18 +47,32 @@ console.log(results.results);
                 <p className='text-md'>
               
               {/* {description.length > 200 ? description=description.substring(0, 200) : description = description}  */}
-           
-              {description.split(' ').map((word, index) => (
+        
+              {description?.split(' ').map((word, index) => (
 
         <span key={index} className={searchTerm.toLowerCase().includes(word.toLowerCase()) ? 'font-bold' : ''}>
-           {word + " "}
+          {word + " "}
             </span>
       ))}
-               </p>
+              </p>
             
               </a>
             </div>
           ))}
+           </div>
+
+<div className="mt-10 flex items-center justify-center ">
+                {Array.from({ length: Math.ceil(results?.length / resultsPerPage) }, (_, i) => i + 1).map((pageNumber) => (
+                    <button
+                        key={pageNumber}
+                        className={currentPage === pageNumber ? 'font-bold ml-4 bg-green-500 w-8 h-8 rounded-full hover:shadow-lg hover:text-white dark:hover:text-black' : ' bg-gray-200 dark:bg-gray-800 ml-4  w-8 h-8 rounded-full hover:shadow-lg hover:text-white dark:hover:text-black'}
+                        onClick={() => handlePageChange(pageNumber)}
+                    >
+                        {pageNumber}
+                    </button>
+                ))}
+                </div>
+       
         </div>
       );
 
