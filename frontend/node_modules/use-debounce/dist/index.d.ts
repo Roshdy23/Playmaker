f40e55bea7bcee8e538a88c3501e966b59757400
Youssef Roshdy <elroshdy23@gmail.com>
2024-05-11 22:23:19 +0300

    adding packages

---
+++ frontend/node_modules/use-debounce/dist/index.d.ts
@@ -0,0 +1,6 @@
+import useDebounce from './useDebounce';
+import useDebouncedCallback from './useDebouncedCallback';
+import type { CallOptions, ControlFunctions, DebouncedState, Options } from './useDebouncedCallback';
+import useThrottledCallback from './useThrottledCallback';
+export { useDebounce, useDebouncedCallback, useThrottledCallback };
+export { CallOptions, ControlFunctions, DebouncedState, Options };
